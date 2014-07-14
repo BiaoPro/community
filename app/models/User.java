3b1042181ce493b3bb641c87b9662aa822b80652
hci-biao <498651300@qq.com>
2014-07-14 20:18:49 +0800

    添加需求文档

---
+++ app/models/User.java
@@ -0,0 +1,23 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import play.data.validation.Unique;
+import play.db.jpa.GenericModel;
+
+@Entity
+@Table(name = "user")
+public class User extends GenericModel {
+    @Id
+    public String id;
+
+    @Column(name = "account")
+    @Unique
+    public String account;
+
+    @Column(name = "password")
+    public String password;
+}
