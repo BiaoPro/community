cc4b8a06e3f0433ab9b8e9a964f57551f6bccb83
scaujohn <isyourname@163.com>
2014-07-16 14:24:15 +0800

    201407161424johncommit

---
+++ test/ApplicationTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import play.test.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+<<<<<<< HEAD
+import models.*;
+=======
+>>>>>>> ad608050a8df8180026390907722eb76f5ac9475
+
+public class ApplicationTest extends FunctionalTest {
+
+    @Test
+    public void testThatIndexPageWorks() {
+        Response response = GET("/");
+        assertIsOk(response);
+        assertContentType("text/html", response);
+        assertCharset(play.Play.defaultWebEncoding, response);
+    }
+    
+}
---
+++ test/BasicTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import java.util.*;
+import play.test.*;
+<<<<<<< HEAD
+import models.*;
+=======
+>>>>>>> ad608050a8df8180026390907722eb76f5ac9475
+
+public class BasicTest extends UnitTest {
+
+    @Test
+    public void aVeryImportantThingToTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
