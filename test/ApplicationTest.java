9f390f5ffd5d1255fad3167f6cccd2648a082972
hci-biao <498651300@qq.com>
2014-07-20 18:04:33 +0800

    删除冲突代码

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -2,13 +2,8 @@ import org.junit.*;
 import play.test.*;
 import play.mvc.*;
 import play.mvc.Http.*;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import models.*;
-=======
->>>>>>> ad608050a8df8180026390907722eb76f5ac9475
-=======
->>>>>>> ad608050a8df8180026390907722eb76f5ac9475
 
 public class ApplicationTest extends FunctionalTest {
 
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,13 +1,8 @@
 import org.junit.*;
 import java.util.*;
 import play.test.*;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import models.*;
-=======
->>>>>>> ad608050a8df8180026390907722eb76f5ac9475
-=======
->>>>>>> ad608050a8df8180026390907722eb76f5ac9475
 
 public class BasicTest extends UnitTest {
 
