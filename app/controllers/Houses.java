900a5b621a40e8acaa292c81e596ddd6d6bd3e4c
scaujohn <isyourname@163.com>
2014-11-17 14:43:34 +0800

    update the things about House

--- app/controllers/Houses.java
+++ app/controllers/Houses.java
@@ -9,39 +9,46 @@ import models.*;
  * @author zhuangxiangpeng
  */
 public class Houses extends Controller {
-
   
-    public static void index() {
-      render();
+    public static void rentHouses(){
+    	render();
     }
-
-	public static void test(){
-		List<House> h=House.findHouses(null, 1);
-		render(h);
-	}
-	//增加房子信息
-	public static void addHouse(){
-		render();
-	}
-	//保存房子信息
-	public static void saveHouse(House house){
-		house.save();
-		showAllHouse();
-	}
-	//删除房子信息
-	public static void delHouse(String id){
-		House house=House.findById(id);
-		house.delete();
-		test();
-	}
-	//显示房子信息
-	public static void showAllHouse(){
-		List<House> h=House.findHouses(null, 1);
-		render(h);
-	}
-	//修改房子信息
-	public static void updateHouse(){
-		
-	}
+    public static void showHousesInfo(){
+    	render();
+    }
+    public static void houses(){
+    	int id=Integer.parseInt(params.get("id"));
+    	List<House> houseList=House.findHouses("",id);
+    	render(houseList);
+    }
+    public static void addHouse(){
+    	House house=new House();
+    	house.way = params.get("way");
+    	house.indentity=params.get("indentity");
+    	house.contacts=params.get("contacts");
+    	house.phone=params.get("phone");
+    	house.community=params.get("community");   	
+    	house.address=params.get("address");
+    	house.bedRoom=Integer.parseInt(params.get("bedRoom"));
+    	house.livingRoom=Integer.parseInt(params.get("livingRoom"));
+    	house.bathRoom=Integer.parseInt(params.get("bathRoom"));
+    	house.square=Double.parseDouble(params.get("square"));
+    	house.currentFloor=Integer.parseInt(params.get("currentFloor"));
+    	house.countFloor=Integer.parseInt(params.get("countFloor"));
+    	house.decorationSituation=params.get("decorationSituation");
+    	house.classSituation=params.get("classSituation");
+    	//多选家电配备
+    	String[] eqs = params.getAll("equipment");
+    	String equipment="";
+    	for(int i=0;i<eqs.length;i++){
+    		equipment+=eqs[i]+",";
+    	}
+    	house.equipment=equipment;
+    	house.price=Double.parseDouble(params.get("price"));
+    	house.details=params.get("details");
+    	house.photoUrl=params.get("photoUrl");
+    	house.save();
+    	render(house);
+    }
 
 }
