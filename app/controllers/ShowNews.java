5c600718a4545fad464af83c3766b9a53139d89e
kingda <371790550@qq.com>
2014-11-20 18:11:41 +0800

    finish news page

--- app/controllers/ShowNews.java
+++ app/controllers/ShowNews.java
@@ -2,29 +2,74 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import utils.PageBeanFactory;
+
 import java.util.*;
 
 import models.*;
 
 public class ShowNews extends Controller{
 	// 展示新闻主页
-	public static void News(){
+	public static void News(int curpage){
 		//获取文章存在的栏目
 		List newsClassIdsList = News.getNewsClassIdsExist();
-		List newsClasslist = NewsClass.getNewsClassByIds(newsClassIdsList);
+		List newsClassList = NewsClass.getNewsClassByIds(newsClassIdsList);
 		
 		//获取文章存在的日期
 		List newsDateList = News.getNewsDateExist();
 		
+		//获取首页文章
+		PageBeanFactory pageBean= new PageBeanFactory(curpage,7);
+		List currentList = News.getIndexNews(pageBean);
+		int maxPage=pageBean.getMaxPage();
+		int[] maxPageArgs = new int[maxPage];
+		for(int i=0;i<maxPageArgs.length;i++){
+			maxPageArgs[i]=i+1;
+		}
+		int fontPage=curpage-1;
+		if(fontPage==0){
+			fontPage=1;
+		}
+		int nextPage=curpage+1;
+		if(nextPage>maxPage){
+			nextPage=maxPage;
+		}
 		
-		render();
+		render(newsClassList,newsDateList,currentList,maxPageArgs,fontPage,nextPage);
+	}
+	
+	/**
+	 * @description 筛选日期和栏目
+	 */
+	public static void newsFilter(int curpage,String newsCreateDate,String newClassType){
+		//获取文章存在的栏目
+		List newsClassIdsList = News.getNewsClassIdsExist();
+		List newsClassList = NewsClass.getNewsClassByIds(newsClassIdsList);
+		//获取文章存在的日期
+		List newsDateList = News.getNewsDateExist();
+		
+			PageBeanFactory pageBean = new PageBeanFactory(curpage,7);
+			List currentList = News.getNewsByDateAndClass(pageBean, newsCreateDate, newClassType);
+			int maxPage=pageBean.getMaxPage();
+			int[] maxPageArgs = new int[maxPage];
+			for(int i=0;i<maxPageArgs.length;i++){
+				maxPageArgs[i]=i+1;
+			}
+			int fontPage=curpage-1;
+			if(fontPage==0){
+				fontPage=1;
+			}
+			int nextPage=curpage+1;
+			if(nextPage>maxPage){
+				nextPage=maxPage;
+			}
+			render(newsClassList,newsDateList,currentList,fontPage,nextPage,maxPageArgs,newsCreateDate,newClassType);
+		
+		
 	}
 	// 展示单个新闻
-	public static void showNewsInfo(){
+	public static void showNewsInfo(String id){
 		render();
 	}
-	//得到新闻json数据
-	public static void NewsJson(){
-		
-	}
+	
 }
