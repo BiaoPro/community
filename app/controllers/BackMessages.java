82da05132c309892004c62106feccc725e28db8e
hci-biao <498651300@qq.com>
2014-11-22 01:30:14 +0800

    完善查看信息反馈内容列表，增加搜索以及搜索分页功能

--- app/controllers/BackMessages.java
+++ app/controllers/BackMessages.java
@@ -11,31 +11,38 @@ import models.LinkCategory;
 
 public class BackMessages extends Controller {
   
- /**
-  * 显示所有链接类别
-  */
- public static void showBackMessages() {
-     List<BackMessage> list = BackMessage.find("ORDER BY pubTime DESC").fetch();
-     render(list);
- } 
- 
  
  /**
   * 默认每页显示5个活动
-  * 
-  * @param curPage
+  * @param searchKey 关键字
+  * @param curPage 页码
+  * @param perPage 页记录数
   */
- public static void showMessages(Integer curPage) {
+public static void showMessages(String searchKey, Integer curPage, Integer perPage) {
    
-     long total = BackMessage.count();
-     
+     PageBean pageBean = null;
+     List<BackMessage> list = null;   
+     long total = 0;
      if(curPage == null ) curPage = 1;
+     if(perPage == null)  perPage = 5;
+
+     if (StringUtils.isEmpty(searchKey)) {
+         // 非搜索模式
+         total = BackMessage.count();
+         list = BackMessage.find("ORDER BY pubTime DESC").fetch(curPage, perPage);
+     } else {
+         // 搜索模式
+             total = BackMessage
+                     .count("select count(*) from BackMessage where message like ?","%" + searchKey + "%");
+             list = BackMessage
+                     .find("from BackMessage where message like ?","%" + searchKey + "%")
+                     .fetch(curPage,perPage);
+         }
      
-     List<BackMessage> list = BackMessage.find("ORDER BY pubTime DESC").fetch(curPage, 2);
      
-     PageBean pageBean = PageBean.getInstance(curPage, total, 2);
+     pageBean = PageBean.getInstance(curPage, total, perPage);
      
-     render(list, curPage, pageBean);
+     render(list, curPage, perPage, pageBean);
 
  }
 
